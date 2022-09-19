a3032ea249daf13366ed37aecc46a2fc51faccb8
Tisha3005 <ribeiro_thais@hotmail.com>
2022-09-19 20:40:10 -0300

    Calculo de Fatorial

---
+++ src/Fatorial.java
@@ -0,0 +1,15 @@
+public class Fatorial {
+
+    public static void main(String[] args) {
+        int fatorial = 1;
+        int valor = 5;
+
+        for( int i = 1; i <= valor; i++ ) {
+            fatorial *= i;
+
+
+        }
+
+        System.out.println(fatorial);
+    }
+}
